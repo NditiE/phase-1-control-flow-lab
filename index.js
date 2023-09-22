1c4acb0b93826c752aca6019b7ebea754f812344
MarleneElla <ellamarlene54@gmail.com>
2023-09-22 22:08:07 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceinfeet) {
+  if (distanceinfeet <= 400) {
+    return "This one is on me";
+  } else if (distanceinfeet > 2000 && distanceinfeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distanceinfeet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(199));
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('pittsburgh'));
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye";
+  }
 }
+
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thanks for everything'));
